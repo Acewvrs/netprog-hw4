e575d7ff9da245a2eccc3e9f6140f5d869090a35
Jun <song.junyoung64@gmail.com>
2024-11-20 22:35:50 -0500

    initial commit

---
+++ hw4_starter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from concurrent import futures
+import sys  # For sys.argv, sys.exit()
+import socket  # for gethostbyname()
+
+import grpc
+
+import csci4220_hw4_pb2
+import csci4220_hw4_pb2_grpc
+
+def run():
+	if len(sys.argv) != 4:
+		print("Error, correct usage is {} [my id] [my port] [k]".format(sys.argv[0]))
+		sys.exit(-1)
+
+	local_id = int(sys.argv[1])
+	my_port = str(int(sys.argv[2])) # add_insecure_port() will want a string
+	k = int(sys.argv[3])
+	my_hostname = socket.gethostname() # Gets my host name
+	my_address = socket.gethostbyname(my_hostname) # Gets my IP address from my hostname
+
+	# Start gRPC server
+	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+	dht_node = DHTNode(local_id, k)
+	csci4220_hw4_pb2_grpc.add_DHTNodeServicer_to_server(dht_node, server)
+
+	server.add_insecure_port(f'[::]:{my_port}')
+	server.start()
+	print(f"Node {local_id}: gRPC server started on port {my_port}")
+
+	''' Use the following code to convert a hostname to an IP and start a channel
+	Note that every stub needs a channel attached to it
+	When you are done with a channel you should call .close() on the channel.
+	Submitty may kill your program if you have too many file descriptors open
+	at the same time. '''
+	
+	#remote_addr = socket.gethostbyname(remote_addr_string)
+	#remote_port = int(remote_port_string)
+	#channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
+
+if __name__ == '__main__':
+	run()
